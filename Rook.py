d3fd35db3910e77dfcf30493c46c6fc812c34c87
Jonathan Levine <51414422+JonathanALevine@users.noreply.github.com>
2020-03-12 20:55:32 -0400

    Comments to Rook class

--- Rook.py
+++ Rook.py
@@ -16,5 +16,11 @@ class Rook(Piece.Piece):
 		destination_column = destination.getColumnNumber()
 		destination_row = destination.getRowNumber()
 
+
+		# when a rook moves it, the following is true
+		# (1) The row changes but the column does not change
+		# (2) The column changes but the row does not change
+		# (3) Cannot jump over a piece
+
 		return True
 
